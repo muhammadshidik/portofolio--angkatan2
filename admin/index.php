ea10a0fc0cdea914a4c2646f50ecd9dc7491516c
putrawijayaa <utaw21@gmail.com>
2025-05-19 11:02:48 +0700

    First Commit

---
+++ admin/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form | Portofolio Siddiq</title>
+    <link rel=" stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" />
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="login-form mt-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-header">
+                                Login Form
+                            </div>
+                            <div class="card-body">
+                                <form action="" method="post">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Email</label>
+                                        <input type="email" class="form-control" name="email"
+                                            placeholder="Ex:admin@gmail.com">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Password</label>
+                                        <input type="Password" class="form-control" name="email"
+                                            placeholder="Masukkan Password anda..">
+                                    </div>
+                                    <div class="mb-3">
+                                        <button type="submit" class="btn btn-primary">Login</button>
+                                    </div>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
